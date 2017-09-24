e2bca10f32b706c64409ab54fda32e111ea873f8
ulguss <u.gussarova@gmail.com>
2017-09-24 18:52:43 +0300

    1 vaheylesanne

---
+++ 1vaheyl/WeatherAPI/src/test/java/ee/ttu/weather/api/test/WeatherApiTest.java
@@ -0,0 +1,63 @@
+package ee.ttu.weather.api.test;
+
+/**
+ * Created by User on 22.09.2017.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WeatherApiTest {
+
+
+    @Test
+    public void getCurrentWeather(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getThreeDaysWeather(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getHigestTemperature(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getLowestTemperature(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getCoordinatesInRightForm(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getCurrentTemperature(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getWindSpeed(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getHumidity(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getCity() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testConnectionExist() {
+        fail("Not yet implemented");
+    }
+}
