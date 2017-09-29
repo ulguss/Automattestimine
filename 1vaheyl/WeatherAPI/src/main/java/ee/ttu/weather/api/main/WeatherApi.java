84e4073d3cd7d84f2540453431671ea0386b88b6
ulguss <u.gussarova@gmail.com>
2017-09-29 19:58:16 +0300

    testid

--- 1vaheyl/WeatherAPI/src/main/java/ee/ttu/weather/api/main/WeatherApi.java
+++ 1vaheyl/WeatherAPI/src/main/java/ee/ttu/weather/api/main/WeatherApi.java
@@ -5,17 +5,29 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  * Created by User on 25.09.2017.
  */
 public class WeatherApi {
-    public int getCurrentTemperature()
+    public static int getCurrentTemperature()
     {
         throw new NotImplementedException();
     }
 
-    public void getThreeDaysWeather()
+
+
+    public static String getCoordinates()
     {
         throw new NotImplementedException();
     }
 
-    public String getCoordinatesInRightForm()
+    public static int getTodayTemperature(
+    {
+        throw new NotImplementedException();
+    }
+
+    public static  int getTomorrowTemperature()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int getAfterTomorrowTemperature()
     {
         throw new NotImplementedException();
     }
--- 1vaheyl/WeatherAPI/src/test/java/ee/ttu/weather/api/test/WeatherApiTest.java
+++ 1vaheyl/WeatherAPI/src/test/java/ee/ttu/weather/api/test/WeatherApiTest.java
@@ -10,6 +10,8 @@ import ee.ttu.weather.api.main.WeatherApi;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class WeatherApiTest {
     WeatherApi weatherApi;
 
@@ -17,28 +19,85 @@ public class WeatherApiTest {
     public void setUp() throws Exception {
         weatherApi = new WeatherApi();
     }
+
     @Test
-    public void testGetCurrentTemperature(){
-        assertTrue(false);
+    public void testIfCurrentTemperatureIsNotNull(){
+        try{
+            assertNotNull(weatherApi.getCurrentTemperature());
+    }catch(Exception e){
+                fail("Failed because: " + e.getMessage());
+        }}
+
+    @Test
+    public void testGetCurrentTemperature() {
+        try{
+            assertEquals(0, weatherApi.getCurrentTemperature());
+        }catch(Exception e){
+                fail("Failed because: " + e.getMessage());
+            }}
+
+    @Test
+    public void testIfCurrentTemperatureIsRealistic() {
+        try{
+        assertTrue(weatherApi.getCurrentTemperature() < 50 && weatherApi.getCurrentTemperature() > -50  );
+        }catch(Exception e){
+            fail("Failed because: " + e.getMessage());
+        }}
+
+    @Test
+   public void testIfThreeDaysWeatherIsAvailable(){
+        assertNotNull(weatherApi.getTodayTemperature());
+        assertNotNull(weatherApi.getTomorrowTemperature());
+        assertNotNull(weatherApi.getAfterTomorrowTemperature());
+
+   }
+
+    @Test
+    public void testIfMaxTemperatureIsHigherThanMinTemperature() {
+        assertTrue(weatherApi.getTodayTemperature() > weatherApi.getTodayTemperature());
+        assertTrue(weatherApi.getTomorrowTemperature() > weatherApi.getTomorrowTemperature());
+        assertTrue(weatherApi.getAfterTomorrowTemperature() > weatherApi.getAfterTomorrowTemperature());
     }
 
-    @Test
-    public void testGetThreeDaysWeather(){
-        assertTrue(false);
-    }
-
-
-
-    @Test
-    public void testGetCoordinatesInRightForm(){
-        assertTrue(false);
-    }
-
-
+//    @Test
+//    public void testMaxTemp(){
+//
+//    }
+//
+//    @Test
+//    public void testMinTemp(){
+//}
 
     @Test
-    public void testConnectionExist() {
+    public void testIfCoordinatesAreInRightFormat(){
+        try{
+        boolean isRigthForm = weatherApi.getCoordinates().matches("\\d{3}.\\d{3}");
+        assertEquals(isRigthForm, true);
+    }catch(Exception e){
+        fail("Failed because: " + e.getMessage());
+    }}
+
+    @Test
+    public void testIfCoordinatesLenght(){
+        try{
+            assertNotNull(WeatherApi.getCoordinates());
+            assertTrue (WeatherApi.getCoordinates().length()==7);
+        }catch(Exception e){
+            fail("Failed because: " + e.getMessage());
+        }}
+
+    @Test
+    public void testIfCoordinatesHasLatAndLon() {
+        try{
+        assertNotNull(weatherApi.getCoordinates());
+        assertTrue(weatherApi.getCoordinates().contains("lat:"));
+        assertTrue(weatherApi.getCoordinates().contains("lon:"));
+    }catch(Exception e){
+            fail("Failed because: " + e.getMessage());
+        }}
+/*    @Test
+    public void testIfConnectionExist() {
 
         fail("Not yet implemented");
-    }
+    }*/
 }
